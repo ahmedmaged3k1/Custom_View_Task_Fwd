9205c2a74c0537fa4694b109691418f19f5b6be2
ahmedmaged3k1 <60134186+ahmedmaged3k1@users.noreply.github.com>
2022-08-24 02:40:01 +0200

    On selecting radio buttons

--- app/src/main/java/com/udacity/LoadingButton.kt
+++ app/src/main/java/com/udacity/LoadingButton.kt
@@ -2,13 +2,18 @@ package com.udacity
 
 import android.animation.ObjectAnimator
 import android.animation.ValueAnimator
+import android.content.ContentValues.TAG
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.drawable.Drawable
+import android.os.Handler
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.core.content.withStyledAttributes
 import kotlin.properties.Delegates
@@ -16,14 +21,16 @@ import kotlin.properties.Delegates
 
 class LoadingButton @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
-) : View(context, attrs, defStyleAttr) {
+) : View(context, attrs, defStyleAttr)   {
     private var widthSize = 0
     private var heightSize = 0
     private var enabledBackground: Drawable? = null
     private var disabledBackground: Drawable? = null
     private var textColor: Int = 0
     private var text: String = "Download"
-    private  var textPaint: Paint=Paint(Paint.ANTI_ALIAS_FLAG)
+    private var textPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private lateinit var radioButton: RadioGroup
+
 
     private val valueAnimator = ValueAnimator()
 
@@ -33,15 +40,16 @@ class LoadingButton @JvmOverloads constructor(
     }
 
 
+
     init {
         textColor = ContextCompat.getColor(context, android.R.color.background_light)
-        textPaint.color=textColor
+        textPaint.color = textColor
         enabledBackground = ContextCompat.getDrawable(context, R.drawable.round_corners)
         disabledBackground = ContextCompat.getDrawable(context, R.drawable.round_corners_colured)
         isClickable = true
-        background=disabledBackground
-        context.withStyledAttributes(attrs,R.styleable.LoadingButton){
-          //  text= getString(R.styleable.LoadingButton_text).toString()
+        background = disabledBackground
+        context.withStyledAttributes(attrs, R.styleable.LoadingButton) {
+            //  text= getString(R.styleable.LoadingButton_text).toString()
         }
     }
 
@@ -49,40 +57,60 @@ class LoadingButton @JvmOverloads constructor(
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         canvas.apply {
-            textPaint.textSize=85f
-            canvas.drawText(text,320f,105f,textPaint)
+            textPaint.textSize = 85f
+            canvas.drawText(text, 320f, 105f, textPaint)
 
 
         }
-
-
 
 
     }
 
     override fun performClick(): Boolean {
+        super.performClick()
         val animator = ObjectAnimator.ofFloat(this, View.ROTATION, -360f, 0f)
-       // val animator2 = ObjectAnimator.of(this.textPaint, View.ALPHA, -360f, 0f)
+        // val animator2 = ObjectAnimator.of(this.textPaint, View.ALPHA, -360f, 0f)
 
         animator.duration = 1000
         animator.start()
         if (background.equals(enabledBackground)) {
             background = disabledBackground
-            textColor=Color.BLACK
-            textPaint.color=textColor
-            text="Download"
+            textColor = Color.BLACK
+            textPaint.color = textColor
+            text = "Download"
 
 
-
-        } else
-        {
+        } else {
             background = enabledBackground
-            text="Loading ..."
+            text = "Loading ..."
 
-            textColor=Color.RED
-            textPaint.color=textColor
+            textColor = Color.RED
+            textPaint.color = textColor
 
         }
+
+        Handler().postDelayed({
+            val animator = ObjectAnimator.ofFloat(this, View.ROTATION, -360f, 0f)
+            // val animator2 = ObjectAnimator.of(this.textPaint, View.ALPHA, -360f, 0f)
+
+            animator.duration = 1000
+            animator.start()
+            if (background.equals(enabledBackground)) {
+                background = disabledBackground
+                textColor = Color.BLACK
+                textPaint.color = textColor
+                text = "Download"
+
+
+            } else {
+                background = enabledBackground
+                text = "Loading ..."
+
+                textColor = Color.RED
+                textPaint.color = textColor
+
+            }
+        }, 1700)
 
         invalidate()
         return true
@@ -100,6 +128,7 @@ class LoadingButton @JvmOverloads constructor(
         heightSize = h
         setMeasuredDimension(w, h)
     }
+
 
 
 
--- app/src/main/java/com/udacity/MainActivity.kt
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.udacity
 
+
 import android.app.DownloadManager
 import android.app.NotificationManager
 import android.app.PendingIntent
@@ -9,10 +10,13 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.net.Uri
 import android.os.Bundle
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
+import kotlinx.android.synthetic.main.content_main.view.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -22,18 +26,39 @@ class MainActivity : AppCompatActivity() {
     private lateinit var notificationManager: NotificationManager
     private lateinit var pendingIntent: PendingIntent
     private lateinit var action: NotificationCompat.Action
+    private lateinit var button: LoadingButton
+    private lateinit var radioButton: RadioGroup
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
+        button = findViewById(R.id.custom_button)
+        radioButton=findViewById(R.id.radioGroup)
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
         custom_button.setOnClickListener {
-            download()
+            if (radioButton.checkedRadioButtonId != -1) {
+                val toast = Toast.makeText(applicationContext, "Downloading", Toast.LENGTH_SHORT)
+                toast.show()
+                when(radioButton.checkedRadioButtonId){
+                    1-> download(URL)
+                    2-> download(URL3)
+                    3-> download(URL2)
+
+
+                }
+
+            } else {
+                val toast = Toast.makeText(applicationContext, "Please Select Item To Download", Toast.LENGTH_SHORT)
+                toast.show()
+
+            }
+
+
         }
     }
+
 
     private val receiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
@@ -41,9 +66,10 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun download() {
+
+    private fun download(url : String ) {
         val request =
-            DownloadManager.Request(Uri.parse(URL))
+            DownloadManager.Request(Uri.parse(url))
                 .setTitle(getString(R.string.app_name))
                 .setDescription(getString(R.string.app_description))
                 .setRequiresCharging(false)
@@ -58,7 +84,12 @@ class MainActivity : AppCompatActivity() {
     companion object {
         private const val URL =
             "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
+        private const val URL2 =
+            "https://github.com/square/retrofit/archive/refs/heads/master.zip"
+        private const val URL3 =
+            "https://github.com/bumptech/glide/archive/refs/heads/master.zip"
         private const val CHANNEL_ID = "channelId"
     }
+
 
 }
